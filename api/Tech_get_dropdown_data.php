9e9d31da847aeb1aac78e82f8abd450b53238deb
L-FEE <106765030@yru.ac.th>
2026-03-27 13:46:19 +0700

    config/db

--- api/Tech_get_dropdown_data.php
+++ api/Tech_get_dropdown_data.php
@@ -7,8 +7,7 @@
 */
 
 session_start();
-// require_once '../config/db.php';
-require_once __DIR__ . '/../config/db.php';
+require_once '../config/db.php';
 header('Content-Type: application/json');
 
 // ตรวจสอบสิทธิ์
